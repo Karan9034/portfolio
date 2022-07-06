a9d885cb3ef4f706e83c46c378b8e84d6876505b
Karan9034 <karan.agr9034@gmail.com>
2022-07-06 22:28:55 +0530

    sus

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -2,10 +2,11 @@ import { Image, Container } from "react-bootstrap";
 
 function HeroSection() {
     return (
-        <Container className='hero-img'>
+        <Container className='hero'>
                 <Image src="/profile.jpg" roundedCircle thumbnail></Image>
                 <div>
-                    <h2>Kon&rsquo;nichiwa! This is Karan.</h2>
+                    <h2>Kon&rsquo;nichiwa, this is <span>Karan Agrawal</span> from India.</h2>
+                    <h3>Full Stack Developer and DevOps Enthusiast</h3>
                 </div>
         </Container>
     );
--- components/SiteNavBar.js
+++ components/SiteNavBar.js
@@ -1,10 +1,10 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 
 const SiteNavBar = () => {
-	return (
+    return (
 		<>
 			<Navbar collapseOnSelect expand="lg" variant="dark" fixed="top" className="site-navbar">
-                <Container fluid="lg">
+                <Container>
                     <Navbar.Brand href="#home">Karan A.</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
