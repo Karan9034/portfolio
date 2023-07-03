da2b197038f03634781930820279b6e5101f5834
Karan Agrawal <karan.agr9034@gmail.com>
2023-07-04 02:21:28 +0530

    navbar fix 2

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import SiteNavBar from './SiteNavBar.js'
+import SiteNavBar from '../components/SiteNavBar.js'
 import HeroSection from '../components/HeroSection.js';
 import Skills from '../components/Skills.js';
 import Work from '../components/Work.js';
